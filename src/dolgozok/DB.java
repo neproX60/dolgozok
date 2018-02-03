0aa9d58849994b51e44418844bf1b020f0a05a8f
neproX60 <t1@122HP3>
2018-02-03 11:20:31 +0100

    Törlés

--- src/dolgozok/DB.java
+++ src/dolgozok/DB.java
@@ -12,63 +12,65 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class DB {
-    
+
     Connection kapcs;
     PreparedStatement ekpar;
     ResultSet eredmeny;
-    
+
     final String dbUrl = "jdbc:mysql://localhost:3306/"
             + "?useUnicode=true&characterEncoding=UTF-8";
     final String user = "root";
     final String pass = "";
-    
+
     public DB() {
         String s1 = "CREATE DATABASE IF NOT EXISTS dolgozok";
         String s2 = "USE dolgozok";
-        String s3 = "CREATE TABLE IF NOT EXISTS adatok (" +
-                    "id int(4) NOT NULL AUTO_INCREMENT," +
-                    "nev varchar(50)," +
-                    "szulido date," +
-                    "fizetes int(7)," +
-                    "PRIMARY KEY(id) " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" +
-                    " COLLATE=utf8mb4_hungarian_ci;";
-        
+        String s3 = "CREATE TABLE IF NOT EXISTS adatok ("
+                + "id int(4) NOT NULL AUTO_INCREMENT,"
+                + "nev varchar(50),"
+                + "szulido date,"
+                + "fizetes int(7),"
+                + "PRIMARY KEY(id) "
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"
+                + " COLLATE=utf8mb4_hungarian_ci;";
+
         try {
             // kapcsolódás a serverhez
             kapcs = DriverManager.getConnection(dbUrl, user, pass);
-            
+
             // adatbázis létrehozása
             ekpar = kapcs.prepareStatement(s1);
             ekpar.execute();
-            
+
             // adatbázis kijelölése
             ekpar = kapcs.prepareStatement(s2);
             ekpar.execute();
-            
+
             // tábla létrehozása
             ekpar = kapcs.prepareStatement(s3);
             ekpar.execute();
-            
+
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
+
     public void lista() {
         try {
             ekpar = kapcs.prepareStatement("SELECT * FROM adatok");
             eredmeny = ekpar.executeQuery();
             while (eredmeny.next()) {
                 System.out.printf("%2d %-50s %s %10d\n",
-                                    eredmeny.getInt("id"),
-                                    eredmeny.getString("nev"),
-                                    eredmeny.getDate("szulido"),
-                                    eredmeny.getInt("fizetes"));
+                        eredmeny.getInt("id"),
+                        eredmeny.getString("nev"),
+                        eredmeny.getDate("szulido"),
+                        eredmeny.getInt("fizetes"));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
+
     public void uj(String nev, String szulido, int fizetes) {
         String s = "INSERT INTO adatok (nev, szulido, fizetes) "
                 + "VALUES (?, ?, ?);";
@@ -83,25 +85,27 @@ public class DB {
             System.out.println(ex.getMessage());
         }
     }
+
     public void beolvas(String fnev) {
         try (Scanner be = new Scanner(new File(fnev))) {
             while (be.hasNextLine()) {
                 String[] sor = be.nextLine().split(",");
-                uj(sor[0],sor[1],Integer.parseInt(sor[2]));
+                uj(sor[0], sor[1], Integer.parseInt(sor[2]));
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
+
     public void kiir(String fnev) {
         try (PrintWriter ki = new PrintWriter(fnev)) {
             try {
                 ekpar = kapcs.prepareStatement("SELECT * FROM adatok");
                 eredmeny = ekpar.executeQuery();
                 while (eredmeny.next()) {
-                    ki.println(eredmeny.getString("nev") + "," +
-                               eredmeny.getDate("szulido") + "," +
-                               eredmeny.getInt("fizetes"));
+                    ki.println(eredmeny.getString("nev") + ","
+                            + eredmeny.getDate("szulido") + ","
+                            + eredmeny.getInt("fizetes"));
                 }
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());;
@@ -110,6 +114,7 @@ public class DB {
             System.out.println(ex.getMessage());
         }
     }
+
     public void emel(int szazalek) {
         String s = "UPDATE adatok SET fizetes = (1+?/100)*fizetes;";
         try {
@@ -117,6 +122,17 @@ public class DB {
             ekpar.setInt(1, szazalek);
             int sorok = ekpar.executeUpdate();
             System.out.println(sorok + " sor módosítva.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void torol() {
+        String s = "DELETE FROM adatok;";
+        try {
+            ekpar = kapcs.prepareStatement(s);
+            int sorok = ekpar.executeUpdate();
+            System.out.println(sorok + " sor törölve.");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
--- src/dolgozok/Dolgozok.java
+++ src/dolgozok/Dolgozok.java
@@ -6,14 +6,14 @@ public class Dolgozok {
 
     public static void main(String[] args) {
         DB ab = new DB();
-        Scanner bill = new Scanner(System.in,"cp1250");
+        Scanner bill = new Scanner(System.in, "cp1250");
         boolean tovabb = true;
         String nev, szulido, fnev;
         int fizetes;
-        
+
         while (tovabb) {
             System.out.println("1-listázás 2-új 3-importálás 4-exportálás "
-                            + "5-emelés 6-törlés 0-kilépés");
+                    + "5-emelés 6-törlés 0-kilépés");
             System.out.print("Válasz: ");
             String v = bill.nextLine();
             switch (v.charAt(0)) {
@@ -45,6 +45,13 @@ public class Dolgozok {
                     int sz = bill.nextInt();
                     bill.nextLine();
                     ab.emel(sz);
+                    break;
+                case '6':
+                    System.out.print("Törölni szeretnél minden rekordot?(i/n): ");
+                    char igen = bill.nextLine().charAt(0);
+                    if (igen == 'i') {
+                        ab.torol();
+                    }
                     break;
                 default:
                     tovabb = false;
