8612fa3a329327aba97a22b13e84c7361f70cf95
neproX60 <t1@122HP3>
2018-02-03 09:27:40 +0100

    Kapcsolódás

--- src/dolgozok/DB.java
+++ src/dolgozok/DB.java
@@ -1,8 +1,10 @@
 package dolgozok;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DB {
     
@@ -14,4 +16,14 @@ public class DB {
             + "?useUnicode=true&characterEncoding=UTF-8";
     final String user = "root";
     final String pass = "";
+    
+    public DB() {
+        try {
+            // kapcsolódás
+            kapcs = DriverManager.getConnection(dbUrl, user, pass);
+            System.out.println("A kapcsolat létrejött.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/dolgozok/Dolgozok.java
+++ src/dolgozok/Dolgozok.java
@@ -3,7 +3,7 @@ package dolgozok;
 public class Dolgozok {
 
     public static void main(String[] args) {
-        
+        DB ab = new DB();
     }
     
 }
