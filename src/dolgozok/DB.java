ba8f69b23266dd85606359bcea8dc869fbe783f3
neproX60 <t1@122HP3>
2018-02-03 10:20:15 +0100

    Listázás

--- src/dolgozok/DB.java
+++ src/dolgozok/DB.java
@@ -49,4 +49,33 @@ public class DB {
             System.out.println(ex.getMessage());
         }
     }
+    public void lista() {
+        try {
+            ekpar = kapcs.prepareStatement("SELECT * FROM adatok");
+            eredmeny = ekpar.executeQuery();
+            while (eredmeny.next()) {
+                System.out.printf("%2d %-50s %s %10d\n",
+                                    eredmeny.getInt("id"),
+                                    eredmeny.getString("nev"),
+                                    eredmeny.getDate("szulido"),
+                                    eredmeny.getInt("fizetes"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void uj(String nev, String szulido, int fizetes) {
+        String s = "INSERT INTO adatok (nev, szulido, fizetes) "
+                + "VALUES (?, ?, ?);";
+        try {
+            ekpar = kapcs.prepareStatement(s);
+            ekpar.setString(1, nev);
+            ekpar.setString(2, szulido);
+            ekpar.setInt(3, fizetes);
+            ekpar.executeUpdate();
+            System.out.println(nev + " hozzáadva.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
